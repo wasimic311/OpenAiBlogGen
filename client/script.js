61b3c13708a06d5a1f2e5bc8b592b6964ee94d73
Wasim <wasim.ic311@gmail.com>
2023-01-06 06:35:37 +0100

    Bug fixed the API is ready to use POST.

---
+++ client/script.js
@@ -0,0 +1,18 @@
+// const input = 'Crypto';
+// const api = 'http://localhost:5000/';
+
+// fetch(api, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             prompt: input
+//         })
+//     })
+//     .then(res => res.json())
+//     const data = data.bot.trim();
+
+//     console.log(data);
+
+
--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config();
-
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -16,35 +15,35 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.status(200).send({
-        message: 'Hello from the otherside',
+        message: 'Hello from the otherside'
     })
-});
+})
 
 app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
 
         const response = await openai.createCompletion({
-            model: "code-davinci-002",
-            prompt: `Write a 1000 word article about ${prompt}`,
+            model: "text-davinci-003",
+            prompt: `Write a 100 word article about ${prompt}`,
             temperature: 0,
             max_tokens: 3000,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0,
         });
-
+        
         res.status(200).send({
             bot: response.data.choices[0].text
         })
 
     } catch (error) {
         console.log(error);
-        res.status(500).send({ error })
+        res.status(500).send(error || 'Something went wrong');
     }
-});
+})
 
 
 app.listen(5000, () => { console.log('App is running on port http://localhost:5000') });
