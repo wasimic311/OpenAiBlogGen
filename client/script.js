b9d6d5ac324b4a0ce398ccb4a82429410035090b
Wasim <wasim.ic311@gmail.com>
2023-01-07 01:31:22 +0100

    Adding Insert function to the frontend (First the sicript (script.js) POST the prompt to the OpenAI server then get the response, and finally it POSTS the response to the blogging website at the end point url/posts)

--- client/script.js
+++ client/script.js
@@ -1,9 +1,13 @@
-const input = 'Crypto';
+const input = '10 Tips for a Successful Morning Routine';
 const api = 'http://localhost:5000/';
+const api2 = 'http://127.0.0.1:3000/posts'
 
-
+let content = '';
 async function fetchData() 
 {
+
+
+//Insert prompt - Method: POST to OPENAI API backend
   let response = await fetch(api, {
     method: 'POST',
     headers: {
@@ -15,15 +19,57 @@ async function fetchData()
 });
   const data = await response.json()
   const parsedData = data.bot.trim()
-  return parsedData;
+  
+
+//Insert new post - Method: POST to FRONTEND Blogging website
+ fetch(api2, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: input,
+      description: parsedData
+
+    })
+  })
+
+
+//return parsedData; //------For TEST-------//
 }
 
-async function main() {
-    let readyData = await fetchData();
-    console.log(readyData);
-  }
+
+
+
+
+
+
+// ---------------------TEST--------------------- //
+// document.getElementById("title").innerHTML = input;
+// async function main() {
+//     let readyData = await fetchData();
+//     // console.log(readyData);
+//     document.getElementById("content").innerHTML = readyData;
+//     return readyData;
+//   }
+
+// main().then(data => { console.log(data) })
+// ---------------------End TEST----------------- //
+
+
+  // Create - Insert new post - Method: POST to FRONTEND Blogging website
+    // fetch(api2, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     title: input,
+    //     description: 'Hi there'//main().then(data => { data })
   
-main();
+    //   })
+    // })
+
 
 
 
